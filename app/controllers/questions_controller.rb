d8fa981feda8b9053215bad281a9dc16ebe2d96e
梅村智己 <s16109tu@sfc.keio.ac.jp>
2017-08-21 21:39:49 +0900

    コミット

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,11 @@ class QuestionsController < ApplicationController
   def index
     @question=Question.all
   end
+
+  def new
+  end
+
+  def create
+    Question.create(name: params[:name], image: params[:image],text: params[:text])
+  end
 end
