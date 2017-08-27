8196c39840c80e2142ce3dbb1e5625172058290a
梅村智己 <s16109tu@sfc.keio.ac.jp>
2017-08-27 16:02:34 +0900

    最終コミット

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,26 @@
 class QuestionsController < ApplicationController
   def index
-    @question=Question.all
+    @questions=Question.all
   end
+
+  def new
+  end
+
+  def create
+    Question.create(name: current_user.name,image: params[:image], text: params[:text],user_id: current_user.id)
+  end
+
+  def destroy
+    question=Question.find(params[:id])
+    if question.user_id==current_user.id
+      question.destroy
+
+    end
+  end
+
+  def show
+    @question=Question.find(params[:id])
+    @comments= Comment.where(question_id: params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
- root 'questions#index'
+  root 'questions#index'
+  devise_for :users
+  resources :questions do
+   resources :comments
+  end
+  resources :users do
+    resources :likes
+  end
+
+  get 'users/:id/followers', to: 'users#follower'
+
 end
